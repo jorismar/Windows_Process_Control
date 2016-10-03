c49a41b6efd7a54d4918fba35c568666cf1f206b
Jorismar <Jorismar@JORISMAR-NOTE>
2016-10-03 00:54:49 -0300

    Finish!

---
+++ process_control/process_control/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = new Process();
+            process.StartInfo.UseShellExecute = true;
+            process.StartInfo.FileName = @"C:\Windows\Notepad.exe";
+            process.StartInfo.Verb = "runas";
+            process.Start();
+            System.Threading.Thread.Sleep(10000);
+            process.Kill();
+            process.WaitForExit();
+        }
+    }
+}
